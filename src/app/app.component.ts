f1b619c4fa0c4d7a1c927bcde3e33ae8761b0b48
Kelvin Primo <kelvin.primo@gmail.com>
2024-09-20 19:59:01 -0300

    Initial commit with Angular 18 boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
 
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PrimeNGConfig } from 'primeng/api';
+import { Aura } from 'primeng/themes/aura';
+import { SelectModule } from 'primeng/select';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DialogModule, ButtonModule, InputTextModule, SelectModule ],
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-boilerplate-primo';
+  animation: boolean = true;
+
+  animations = [
+    { name: 'Fade', code: 'fade' },
+    { name: 'Slide', code: 'slide' },
+    { name: 'Slide Horizontal', code: 'slide' },
+    { name: 'Slide Vertical', code: 'slide' },
+    { name: 'Scale', code: 'scale' }
+  ]
+
+  dynamicAnimationClasses: string = 'p-dialog p-dialog-visible p-dialog-rtl p-dialog-draggable p-dialog-resizable p-dialog-modal p-dialog-focus-trap';
+
+  private PrimeNGConfig = inject(PrimeNGConfig);
+
+  ngOnInit() {
+    this.PrimeNGConfig.theme.set({
+      preset: Aura,
+      options: {
+        prefix: 'p',
+        darkModeSelector: 'system',
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities'
+        }
+      }
+    });
+    this.PrimeNGConfig.ripple.set(true);
+  }
+
+
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
 }
